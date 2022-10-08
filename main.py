d7d07b4e213a7fecab3419ea587718c07e1f41ec
Mac0r0ni_And_Weed <61136278+Mac0r0ni@users.noreply.github.com>
2022-10-08 18:59:52 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+seen = set()
+with open('combined.txt', 'r') as start, open('deduped.txt', 'w') as end:
+    for line in start:
+        h = hash(line)
+        if h not in seen:
+            end.write(line)
+            seen.add(h)
